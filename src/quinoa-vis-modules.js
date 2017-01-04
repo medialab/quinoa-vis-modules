51b798dc7c40750e76f16a84760cf1c799198969
Robin de Mourat <robin.demourat@gmail.com>
2017-01-04 11:38:42 +0100

    add sigma requirement to bundle

--- src/quinoa-vis-modules.js
+++ src/quinoa-vis-modules.js
@@ -1,3 +1,9 @@
+require('../sigma/sigma.min.js');
+require('../sigma.layout.forceAtlas2.min.js');
+require('../sigma.parsers.gexf.min.js');
+require('../sigma.plugins.saveCamera.js');
+
+
 import Graph from './Graph/Graph';
 import Map from './Map/Map';
 import Timeline from './Timeline/Timeline';
