778ddd2e34bac53242aa02c7690fd7c3288b8e5a
Robin de Mourat <robin.demourat@gmail.com>
2017-01-09 11:20:09 +0100

    setup storybook tests for timeline component

--- stories/index.js
+++ stories/index.js
@@ -7,30 +7,67 @@ import Timeline from '../src/Timeline/Timeline';
 import Graph from '../src/Graph/Graph';
 import Map from '../src/Map/Map';
 
+import timelineData from 'dsv!./mock_data/milestones-datavis.csv';
+import graphData from './mock_data/miserables.json';
+import mapData from 'dsv!./mock_data/etablissements-enseignement-fr.csv';
+
+const timelineDataMap = {
+  year: 'year',
+  name: (d) => d.content,
+  category: 'category',
+  endYear: 'end year'
+}
+
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+    <Welcome showApp={linkTo('Timeline')}/>
   ));
 
 storiesOf('Timeline', module)
   .add('default', () => (
-    <Timeline />
-  ));
+    <Timeline 
+      data={timelineData} 
+      dataMap={timelineDataMap}
+      onViewChange={(e) => action('on view change', e)}
+    />
+  ))
+  .add('switch between view states (navigable)', () => (
+    <Timeline 
+      data={timelineData} 
+      dataMap={timelineDataMap}
+      onViewChange={(e) => action('on view change', e)}
+    />
+  ))
+  .add('switch between view states (locked)', () => (
+    <Timeline 
+      data={timelineData} 
+      dataMap={timelineDataMap}
+      onViewChange={(e) => action('on view change', e)}
+    />
+  ))
+  .add('very small', () => (
+    <Timeline 
+      data={timelineData} 
+      dataMap={timelineDataMap}
+      onViewChange={(e) => action('on view change', e)}
+    />
+  ))
+  .add('very big', () => (
+    <Timeline 
+      data={timelineData} 
+      dataMap={timelineDataMap}
+      onViewChange={(e) => action('on view change', e)}
+    />
+  ))
 
+/*
 storiesOf('Graph', module)
   .add('default', () => (
-    <Graph />
+    <Graph data={graphData} />
   ));
 
 storiesOf('Map', module)
   .add('default', () => (
     <Map />
   ));
+  */
