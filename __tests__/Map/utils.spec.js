47c7fdad6b9c8eac3adcedf7ab57e210a704f085
Davy Peter Braun <hello@davybraun.com>
2017-01-23 15:17:07 +0100

    add tests for Map/utils#reverseCoordinates

---
+++ __tests__/Map/utils.spec.js
@@ -0,0 +1,32 @@
+import {
+  reverseCoordinates,
+  flattenGeoJSON,
+  mapData,
+  computeDataRelatedState
+} from '../../src/Map/utils';
+
+describe('Map utils', () => {
+
+  describe('reverseCoordinates', () => {
+    it('should be a function', () => {
+      expect(typeof reverseCoordinates).toBe('function');
+    });
+
+    it('should return an array, given an array argument', () => {
+      expect(Array.isArray(reverseCoordinates([]))).toBe(true);
+    });
+
+    it('should reverse coordinates', () => {
+      const reversed = JSON.stringify(reverseCoordinates([1, 2]));
+      const expected = JSON.stringify([2, 1]);
+      expect(reversed).toBe(expected);
+    });
+
+    it('should reverse recursively', () => {
+      const reversed = JSON.stringify(reverseCoordinates([[1, 2], [3, 4]]));
+      const expected = JSON.stringify([[2, 1], [4, 3]]);
+      expect(reversed).toBe(expected);
+    });
+  });
+
+});
