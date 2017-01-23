fa35195015fd807c6023c232d745b92914eb3546
Robin de Mourat <robin.demourat@gmail.com>
2017-01-23 14:34:43 +0100

    expose utils in lib

--- build/quinoa-vis-modules.js
+++ build/quinoa-vis-modules.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Timline = exports.Map = exports.Network = undefined;
+exports.mapNetworkData = exports.mapTimelineData = exports.mapMapData = exports.parseNetworkData = exports.parseTimelineData = exports.parseMapData = exports.Timline = exports.Map = exports.Network = undefined;
 
 var _Network = require('./Network/Network');
 
@@ -17,6 +17,26 @@ var _Timeline = require('./Timeline/Timeline');
 
 var _Timeline2 = _interopRequireDefault(_Timeline);
 
+var _mapDataMapper = require('./utils/mapDataMapper');
+
+var _mapDataMapper2 = _interopRequireDefault(_mapDataMapper);
+
+var _timelineDataMapper = require('./utils/timelineDataMapper');
+
+var _timelineDataMapper2 = _interopRequireDefault(_timelineDataMapper);
+
+var _networkDataMapper = require('./utils/networkDataMapper');
+
+var _networkDataMapper2 = _interopRequireDefault(_networkDataMapper);
+
+var _timelineDataParser = require('./utils/timelineDataParser');
+
+var _timelineDataParser2 = _interopRequireDefault(_timelineDataParser);
+
+var _networkDataParser = require('./utils/networkDataParser');
+
+var _networkDataParser2 = _interopRequireDefault(_networkDataParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 require('../sigma/sigma.min.js');
@@ -27,3 +47,11 @@ require('../sigma/sigma.plugins.saveCamera.js');
 var Network = exports.Network = _Network2.default;
 var Map = exports.Map = _Map2.default;
 var Timline = exports.Timline = _Timeline2.default;
+
+var parseMapData = exports.parseMapData = _mapDataMapper2.default;
+var parseTimelineData = exports.parseTimelineData = _timelineDataParser2.default;
+var parseNetworkData = exports.parseNetworkData = _networkDataParser2.default;
+
+var mapMapData = exports.mapMapData = _mapDataMapper2.default;
+var mapTimelineData = exports.mapTimelineData = _timelineDataMapper2.default;
+var mapNetworkData = exports.mapNetworkData = _networkDataMapper2.default;
--- src/quinoa-vis-modules.js
+++ src/quinoa-vis-modules.js
@@ -7,6 +7,22 @@ import QuinoaNetwork from './Network/Network';
 import QuinoaMap from './Map/Map';
 import QuinoaTimeline from './Timeline/Timeline';
 
+import QMapDataMapper from './utils/mapDataMapper';
+import QTimelineDataMapper from './utils/timelineDataMapper';
+import QNetworkDataMapper from './utils/networkDataMapper';
+
+import QMapDataParser from './utils/mapDataMapper';
+import QTimelineDataParser from './utils/timelineDataParser';
+import QNetworkDataParser from './utils/networkDataParser';
+
 export const Network = QuinoaNetwork;
 export const Map = QuinoaMap;
 export const Timline = QuinoaTimeline;
+
+export const parseMapData = QMapDataParser;
+export const parseTimelineData = QTimelineDataParser;
+export const parseNetworkData = QNetworkDataParser;
+
+export const mapMapData = QMapDataMapper;
+export const mapTimelineData = QTimelineDataMapper;
+export const mapNetworkData = QNetworkDataMapper;
