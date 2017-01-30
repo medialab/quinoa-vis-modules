b37e75ceb3cb92a992ddec06ee30415898e64c39
Davy Peter Braun <hello@davybraun.com>
2017-01-30 17:41:20 +0100

    enable dangerouslySetInnerHtml, add isomorphic-fetch and es-promise polyfill to deps

--- build/SVGViewer/SVGViewer.js
+++ build/SVGViewer/SVGViewer.js
@@ -19,32 +19,76 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } 
 
 
-var SVGViewer = function (_Component) {
-  _inherits(SVGViewer, _Component);
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+var SVGViewer = function (_React$Component) {
+  _inherits(SVGViewer, _React$Component);
 
   function SVGViewer(props) {
     _classCallCheck(this, SVGViewer);
 
-    return _possibleConstructorReturn(this, (SVGViewer.__proto__ || Object.getPrototypeOf(SVGViewer)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (SVGViewer.__proto__ || Object.getPrototypeOf(SVGViewer)).call(this, props));
+
+    _this.state = { svg: null };
+
+    _this.loadFile = _this.loadFile.bind(_this);
+    _this.parseSVG = _this.parseSVG.bind(_this);
+    return _this;
   }
 
   _createClass(SVGViewer, [{
-    key: 'shouldComponentUpdate',
-    value: function shouldComponentUpdate() {}
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      return this.props.file.indexOf('http') === 0 ? this.loadFile() : this.parseSVG(this.props.svgString);
+    }
+  }, {
+    key: 'loadFile',
+    value: function loadFile() {
+      var _this2 = this;
+
+      fetch(this.props.file).then(function (res) {
+        if (res.status >= 400) {
+          throw new Error('Bad response from server while loading ' + _this2.props.file);
+        }
+        return res.text();
+      }).then(function (svg) {
+        _this2.setState({ svg: _this2.parseSVG(svg) });
+      }).catch(function (err) {
+        throw new Error('Unknown error occured while loading ' + _this2.props.file + ' -> ' + err.message);
+      });
+    }
+  }, {
+    key: 'parseSVG',
+    value: function parseSVG() {
+      var xmlString = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+
+      return new DOMParser().parseFromString(xmlString, 'text/xml');
+    }
   }, {
     key: 'render',
     value: function render() {
-      return _react2.default.createElement(
+      return this.state.svg ? _react2.default.createElement('div', { dangerouslySetInnerHTML: {
+          __html: new XMLSerializer().serializeToString(this.state.svg.documentElement)
+        } }) : _react2.default.createElement(
         'div',
         null,
-        'hello'
+        'Loading...'
       );
     }
   }]);
 
   return SVGViewer;
-}(_react.Component);
+}(_react2.default.Component);
 
-SVGViewer.proptypes = {};
+SVGViewer.defaultProps = {
+  allowUserViewChange: true
+};
+
+SVGViewer.proptypes = {
+  allowUserViewChange: _react.PropTypes.bool,
+  file: _react.PropTypes.string,
+  svgString: _react.PropTypes.string
+};
 
 exports.default = SVGViewer;
