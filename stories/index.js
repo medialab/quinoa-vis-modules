8f637e268223518e5b95248a638946b96e668d73
Robin de Mourat <robin.demourat@gmail.com>
2017-02-13 14:28:21 +0100

    small fix

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Welcome from './Welcome';
 
+
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
     <Welcome showApp={linkTo('Timeline')}/>
